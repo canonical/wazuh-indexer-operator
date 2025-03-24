c5f5ad0a41c4822c5b6c2a46048df76bd9839a87
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-03-24 17:47:28 +0100

    Add terraform module (#9)
    * Add terraform module
    
    * Add terraform module
    
    * Add terraform module
    
    * Add terraform module
    
    (cherry picked from commit 46bd9ebe0ca22dd60d9bba6fc463947f0702eb65)

---
+++ terraform/charm/main.tf
@@ -0,0 +1,20 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+resource "juju_application" "wazuh_indexer" {
+  name  = var.app_name
+  model = var.model
+
+  charm {
+    name     = var.app_name
+    channel  = var.channel
+    revision = var.revision
+    base     = var.base
+  }
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+
+  expose {}
+}
