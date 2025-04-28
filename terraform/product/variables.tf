ce9c019befb3e3eaa67e0656157843316b9f2fd8
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-04-28 10:35:52 +0200

    Deploy grafana agent as part of the TF product (#16)

---
+++ terraform/product/variables.tf
@@ -0,0 +1,30 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+variable "indexer_model" {
+  description = "Reference to the VM Juju model to deploy the indexer charms to."
+  type        = string
+}
+
+variable "grafana_agent" {
+  type = object({
+    app_name    = optional(string, "grafana-agent")
+    channel     = optional(string, "latest/stable")
+    config      = optional(map(string), {})
+    constraints = optional(string, "arch=amd64")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+  })
+}
+
+variable "wazuh_indexer" {
+  type = object({
+    app_name    = optional(string, "wazuh-indexer")
+    channel     = optional(string, "4.9/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "arch=amd64")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+    units       = optional(number, 3)
+  })
+}
