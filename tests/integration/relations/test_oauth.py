7c9faf62a37c997c8b22bbab8f6f22a0beccd6ac
Sebastien Georget <sebastien.georget@canonical.com>
2025-06-13 08:20:56 +0200

    Troubleshoot tests

--- tests/integration/relations/test_oauth.py
+++ tests/integration/relations/test_oauth.py
@@ -134,6 +134,8 @@ async def test_oauth_access(ops_test: OpsTest, microk8s_model: Model):
     result = requests.get(
         opensearch_url, headers={"Authorization": f"Bearer {oauth_access_token}"}, verify=False
     )
+    logger.warning(f"DEBUG: {result.status_code}")
+    logger.warning(f"DEBUG: {result.content}")
     assert result.json().get("status") == 403, "no permissions error expected"
 
     action = (
